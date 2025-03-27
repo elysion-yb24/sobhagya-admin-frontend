f4b3fb9562b5c68bcda9bd46eb44990198167b33
arnav-elysion <arnav@elysionsoftwares.in>
2025-03-27 16:33:16 +0530

    Changes done

--- app/(auth)/pages/coming-soon-boxed/page.tsx
+++ app/(auth)/pages/coming-soon-boxed/page.tsx
@@ -10,7 +10,7 @@ const ComingSoonBoxed = () => {
     return (
         <div>
             <div className="absolute inset-0">
-                <img src="/assets/images/auth/bg-gradient.png" alt="image" className="h-full w-full object-cover" />
+                <img src="/assets/images/auth/bg-gradient.png"  className="h-full w-full object-cover" />
             </div>
             <div className="relative flex min-h-screen items-center justify-center bg-[url(/assets/images/auth/map.png)] bg-cover bg-center bg-no-repeat px-6 py-10 dark:bg-[#060818] sm:px-16">
                 <img src="/assets/images/auth/coming-soon-object1.png" alt="image" className="absolute left-0 top-1/2 h-full max-h-[893px] -translate-y-1/2" />
